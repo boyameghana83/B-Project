895916d5d42360f9c2ddaab7c9c1aa94a831e0e6
boyameghana83 <boyameghana83@gmail.com>
2024-11-01 17:59:11 +0530

    Update demo.sh
    dear team

--- demo.sh
+++ demo.sh
@@ -4,3 +4,7 @@ welcome again to the git devops session
 welcome again to the git devops sessions
 welcome again to the git devops sessions
 :wq!
+welcome again to the git devops session
+welcome again to the git devops sessions
+welcome again to the git devops sessions
+:wq!
